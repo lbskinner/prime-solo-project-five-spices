8055699db4341baec29070a317a903ed3c1ed8a9
Leila Skinner <beileiwang@gmail.com>
2020-05-04 10:59:38 -0500

    add on delete cascade to tables

--- database.sql
+++ database.sql
@@ -13,9 +13,34 @@ CREATE TABLE "user" (
 CREATE TABLE "recipe" (
   "recipe_id" SERIAL PRIMARY KEY,
   "recipe_name" VARCHAR (255) NOT NULL,
-  "description" VARCHAR (800),
+  "description" VARCHAR (1000),
   "total_time" VARCHAR (80),
   "serving_size" VARCHAR (80),
   "favorite" BOOLEAN,
-  "user_id" INT REFERENCES "user"
+  "user_id" INT REFERENCES "user" ON DELETE CASCADE
 );
+
+CREATE TABLE "ingredient" (
+  "ingredient_id" SERIAL PRIMARY KEY,
+  "ingredient_item" VARCHAR (255) NOT NULL,
+  "recipe_id" INT REFERENCES "recipe" ON DELETE CASCADE
+);
+
+CREATE TABLE "instruction" (
+  "instruction_id" SERIAL PRIMARY KEY,
+  "instruction_number" INTEGER,
+  "instruction_descrption" VARCHAR (1000) NOT NULL,
+  "recipe_id" INT REFERENCES "recipe" ON DELETE CASCADE
+);
+
+CREATE TABLE "category" (
+  "category_id" SERIAL PRIMARY KEY,
+  "category_name" VARCHAR (80)
+);
+
+CREATE TABLE "recipe_category" (
+  "recipe_category_id" SERIAL PRIMARY KEY,
+  "category_id" INT REFERENCES "category",
+  "recipe_id" INT REFERENCES "recipe" ON DELETE CASCADE
+);
+
