1bc411a3ad1b2338e1cf7a0e52b283df782445d7
Leila Skinner <beileiwang@gmail.com>
2020-05-04 15:09:11 -0500

    get favorite recipes route created and tested in postman

--- server/routes/recipe.router.js
+++ server/routes/recipe.router.js
@@ -19,6 +19,37 @@ router.get("/", (req, res) => {
 });
 
 /**
+ * GET favorite recipes for home page list
+ */
+router.get("/favorite", (req, res) => {
+  const queryText = `SELECT * FROM "recipe" WHERE "favorite" = true ORDER BY "recipe_name" ASC;`;
+  pool
+    .query(queryText)
+    .then((responseFromDb) => {
+      res.send(responseFromDb.rows);
+    })
+    .catch((error) => {
+      console.log("Get All Recipes Error: ", error);
+      res.sendStatus(500);
+    });
+});
+
+/**
+ * GET all recipes by category
+ */
+router.get("/", (req, res) => {
+  const queryText = `SELECT * FROM "recipe" ORDER BY "recipe_name" ASC;`;
+  pool
+    .query(queryText)
+    .then((responseFromDb) => {
+      res.send(responseFromDb.rows);
+    })
+    .catch((error) => {
+      console.log("Get All Recipes Error: ", error);
+      res.sendStatus(500);
+    });
+});
+/**
  * POST route template
  */
 router.post("/", (req, res) => {});
