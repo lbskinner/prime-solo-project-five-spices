a3cb876bbe1b2c558d817f89173405d7b3a4dead
Leila Skinner <beileiwang@gmail.com>
2020-05-04 10:36:36 -0500

    tables created

--- database.sql
+++ database.sql
@@ -6,5 +6,16 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
+    "email" VARCHAR (255) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "recipe" (
+  "recipe_id" SERIAL PRIMARY KEY,
+  "recipe_name" VARCHAR (255) NOT NULL,
+  "description" VARCHAR (800),
+  "total_time" VARCHAR (80),
+  "serving_size" VARCHAR (80),
+  "favorite" BOOLEAN,
+  "user_id" INT REFERENCES "user"
 );
