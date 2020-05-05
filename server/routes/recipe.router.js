95939cf381d92c84f7d26cce81967bb79c3dd529
Leila Skinner <beileiwang@gmail.com>
2020-05-05 14:49:51 -0500

    post route to add recipe created, not tested yet

--- server/routes/recipe.router.js
+++ server/routes/recipe.router.js
@@ -100,6 +100,51 @@ router.get("/details-instructions/:id", (req, res) => {
 router.post("/", (req, res) => {});
 
 /**
+ * POST new recipe
+ */
+router.post("/", async (req, res) => {
+  const newRecipeData = req.body;
+  //   let ingredientArray = [];
+  //   for (let i = 1; i <= newRecipeData.ingredient.length; i++) {
+  //     item = `$${i}`;
+  //     ingredientArray.push(item);
+  //   }
+  const queryTextRecipe = `INSERT INTO "recipes" ("recipe_name", "description", "total_time", serving_size", "user_id", "image_url", "recipe_url")
+    VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING "recipe_id";`;
+  const savedRecipeDate = await pool.query(queryTextRecipe, [
+    newRecipeData.name,
+    newRecipeData.description,
+    newRecipeData.time,
+    newRecipeData.serving,
+    newRecipeData.user_id,
+    newRecipeData.image_url,
+    newRecipeData.recipe_url,
+  ]);
+  const newIngredientArray = newRecipeData.ingredient.map((item, index) => {
+    return `('${item}', ${savedRecipeDate.rows[0].recipe_id})`;
+  });
+  const queryTextIngredient = `INSERT INTO "ingredient" ("ingredient_item", "recipe_id") 
+      VALUES (${newIngredientArray.join(".")});`;
+  const savedIngredientData = await pool.query(queryTextIngredient);
+  const newInstructionArray = newRecipeData.instruction.map((step, index) => {
+    return `(${step.instruction_number}, '${step.instruction_description}', ${savedRecipeDate.rows[0].recipe_id})`;
+  });
+  const queryTextInstruction = `INSERT INTO "instruction" ("instruction_number", "instruction_description", "recipe_id")
+    VALUES (${newInstructionArray.join(",")});`;
+
+  const savedInstructionData = await pool.query(queryTextInstruction);
+  res.sendStatus(201);
+
+  // .then((responseFromDb) => {
+  //   const recipeId = responseFromDb.rows[0].recipe_id;
+  //   pool.query(queryTextIngredient);
+  // })
+  // .catch((error) => {
+  //   console.log("Post New Recipe Error: ", error);
+  // });
+});
+
+/**
  *DELETE individual recipe by recipe id
  */
 router.delete("/:id", (req, res) => {
