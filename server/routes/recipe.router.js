7f1880c6d2f3d22fffa265aca6a09f4e75045163
Leila Skinner <beileiwang@gmail.com>
2020-05-05 12:59:38 -0500

    split category related routers to its own router file

--- server/routes/recipe.router.js
+++ server/routes/recipe.router.js
@@ -35,25 +35,6 @@ router.get("/favorite", (req, res) => {
 });
 
 /**
- * GET all recipes by category
- */
-router.get("/category/:id", (req, res) => {
-  // id on params is category id
-  const categoryId = req.params.id;
-  const queryText = `SELECT * FROM "recipe" JOIN "recipe_category" ON "recipe".recipe_id = "recipe_category".recipe_id
-  WHERE "recipe_category".category_id = $1 ORDER BY "recipe_name" ASC;`;
-  pool
-    .query(queryText, [categoryId])
-    .then((responseFromDb) => {
-      res.send(responseFromDb.rows);
-    })
-    .catch((error) => {
-      console.log("Get Recipes By Category Error: ", error);
-      res.sendStatus(500);
-    });
-});
-
-/**
  * GET recipes by search keywords
  */
 router.get("/search", (req, res) => {
@@ -114,82 +95,8 @@ router.get("/details-instructions/:id", (req, res) => {
 });
 
 /**
- * GET categories for individual recipe details page
- */
-router.get("/details-categories/:id", (req, res) => {
-  // id on params us recipe id
-  const recipeId = req.params.id;
-  console.log(recipeId);
-
-  const queryText = `SELECT "category".category_id, "category".category_name, "recipe_category".recipe_id, "recipe_category".recipe_category_id
-  FROM "category" JOIN "recipe_category" ON "category".category_id = "recipe_category".category_id
-  WHERE "recipe_category".recipe_id = $1;`;
-  pool
-    .query(queryText, [recipeId])
-    .then((responseFromDb) => {
-      res.send(responseFromDb.rows);
-    })
-    .catch((error) => {
-      console.log("Get Category for Recipe Details Error: ", error);
-      res.sendStatus(500);
-    });
-});
-
-/**
- * GET category list for individual recipe details page select box
- */
-router.get("/category", (req, res) => {
-  const queryText = `SELECT * FROM "category";`;
-  pool
-    .query(queryText)
-    .then((responseFromDb) => {
-      res.send(responseFromDb.rows);
-    })
-    .catch((error) => {
-      console.log("Get All Categories Error: ", error);
-      res.sendStatus(500);
-    });
-});
-
-/**
  * POST route template
  */
 router.post("/", (req, res) => {});
-
-/**
- * POST category to individual recipe
- */
-router.post("/details-categories", (req, res) => {
-  const recipeCategoryDate = req.body;
-  const queryText = `INSERT INTO "recipe_category" ("category_id", "recipe_id")
-    VALUES ($1, $2);`;
-  pool
-    .query(queryText, [
-      recipeCategoryDate.category_id,
-      recipeCategoryDate.recipe_id,
-    ])
-    .then(() => res.sendStatus(201))
-    .catch((error) => {
-      console.log("Post Recipe Category Error: ", error);
-      res.sendStatus(500);
-    });
-});
-
-/**
- * DELETE category from individual recipe
- */
-router.delete("/details-categories/:id", (req, res) => {
-  // id on params is recipe_category_id
-  const recipeCategoryId = req.params.id;
-  console.log(recipeCategoryId);
-  const queryText = `DELETE FROM "recipe_category" WHERE "recipe_category_id" = $1`;
-  pool
-    .query(queryText, [recipeCategoryId])
-    .then(() => res.sendStatus(200))
-    .catch((error) => {
-      console.log("Delete Recipe Category Error: ", error);
-      res.sendStatus(500);
-    });
-});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require("./strategies/user.strategy");
 // Route includes
 const userRouter = require("./routes/user.router");
 const recipeRouter = require("./routes/recipe.router");
+const categoryRouter = require("./routes/category.router");
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,7 +25,8 @@ app.use(passport.session());
 
 /* Routes */
 app.use("/api/user", userRouter);
-app.use("/api/recipes", recipeRouter);
+app.use("/api/recipe", recipeRouter);
+app.use("/api/category", categoryRouter);
 
 // Serve static files
 app.use(express.static("build"));
