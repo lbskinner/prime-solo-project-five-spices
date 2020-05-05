7f1ff25874c5d1cedc7e7120b71e10864d728efc
Leila Skinner <beileiwang@gmail.com>
2020-05-04 20:37:49 -0500

    recipes details not fully working yet

--- server/routes/recipe.router.js
+++ server/routes/recipe.router.js
@@ -72,6 +72,46 @@ router.get("/search", (req, res) => {
       res.sendStatus(500);
     });
 });
+
+/**
+ * GET individual recipe details, need more work
+ */
+router.get("/:id", (req, res) => {
+  const recipeId = req.params.id;
+  const queryText = `SELECT "recipe".*, array_agg("ingredient".ingredient_item), json_agg(json_build_object('instruction_num', "instruction".instruction_number, 'instruction_descrip', "instruction".instruction_descrption)) as "instructions" FROM "recipe" JOIN "ingredient" ON "recipe".recipe_id = "ingredient".recipe_id JOIN "instruction" on "recipe".recipe_id = "instruction".recipe_id 
+  WHERE "recipe".recipe_id = $1 GROUP BY "recipe".recipe_id;`;
+  pool
+    .query(queryText, [recipeId])
+    .then((responseFromDb) => {
+      res.send(responseFromDb.rows);
+    })
+    .catch((error) => {
+      console.log("Get All Recipes Error: ", error);
+      res.sendStatus(500);
+    });
+});
+
+/**
+ * GET category list for individual recipe details page
+ */
+router.get("/search", (req, res) => {
+  const searchWords = req.query.q;
+  console.log(searchWords);
+
+  const queryText = `SELECT "recipe".recipe_id, "recipe".recipe_name, "recipe".description, array_agg("ingredient".ingredient_item) FROM "recipe"
+      JOIN "ingredient" ON "recipe".recipe_id = "ingredient".recipe_id WHERE "recipe".recipe_name LIKE $1 OR
+      "recipe".description LIKE $1 OR "ingredient".ingredient_item LIKE $1 GROUP BY "recipe".recipe_id;`;
+  pool
+    .query(queryText, [`%${searchWords}%`])
+    .then((responseFromDb) => {
+      res.send(responseFromDb.rows);
+    })
+    .catch((error) => {
+      console.log("Get All Recipes Error: ", error);
+      res.sendStatus(500);
+    });
+});
+
 /**
  * POST route template
  */
