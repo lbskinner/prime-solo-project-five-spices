a1f1f5f354074ae02b177b4436c1f37dfdeac2fe
Leila Skinner <beileiwang@gmail.com>
2020-05-09 17:25:53 -0500

    changed layout of details page

--- src/components/DetailsPage/RecipeDetailsPage/RecipeDetailsPage.js
+++ src/components/DetailsPage/RecipeDetailsPage/RecipeDetailsPage.js
@@ -12,34 +12,38 @@ class RecipeDetailsPage extends Component {
   render() {
     const recipe = this.props.recipeDetails[0];
     return (
-      <Grid container spacing={3}>
-        <Grid item xs={8}>
-          <Link
-            component="a"
-            href={recipe.recipe_url}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Original Recipe Link
-          </Link>
-          <Typography variant="h3">{recipe.recipe_name}</Typography>
-          <Typography>{`Total Cook Time: ${recipe.total_time}`}</Typography>
-          <Typography>{`Servings: ${recipe.serving_size}`}</Typography>
-          <Typography>
-            Favorite: <FavoriteButton />
-          </Typography>
-          <Typography>{recipe.description}</Typography>
-          <CategoryDetailsPage />
+      <div>
+        <Link
+          component="a"
+          href={recipe.recipe_url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Original Recipe Link
+        </Link>
+        <Typography variant="h3">{recipe.recipe_name}</Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Typography>{`Total Cook Time: ${recipe.total_time}`}</Typography>
+            <Typography>{`Servings: ${recipe.serving_size}`}</Typography>
+            <Typography>
+              Favorite: <FavoriteButton />
+            </Typography>
+            <Typography>{recipe.description}</Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <CategoryDetailsPage />
+          </Grid>
+          <Grid item xs={4}>
+            <br />
+            <img
+              src={recipe.image_url}
+              alt={recipe.recipe_name}
+              style={{ width: 250, height: 250 }}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <br />
-          <img
-            src={recipe.image_url}
-            alt={recipe.recipe_name}
-            style={{ width: 250, height: 250 }}
-          />
-        </Grid>
-      </Grid>
+      </div>
     );
   }
 }
