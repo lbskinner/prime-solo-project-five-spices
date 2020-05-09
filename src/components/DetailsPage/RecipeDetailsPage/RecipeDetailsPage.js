774b78e4bae125f30b7c5708b9e0236e0a2617ad
Leila Skinner <beileiwang@gmail.com>
2020-05-09 16:21:00 -0500

    use link from material ui

--- src/components/DetailsPage/RecipeDetailsPage/RecipeDetailsPage.js
+++ src/components/DetailsPage/RecipeDetailsPage/RecipeDetailsPage.js
@@ -3,36 +3,37 @@ import { connect } from "react-redux";
 import mapStoreToProps from "../../../redux/mapStoreToProps";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import FavoriteButton from "../FavoriteButtonDetailsPage/FavoriteButtonDetailsPage";
 
 class RecipeDetailsPage extends Component {
   render() {
+    const recipe = this.props.recipeDetails[0];
     return (
       <Grid container spacing={3}>
         <Grid item xs={8}>
-          <a
-            href={this.props.recipeDetails[0].recipe_url}
+          <Link
+            component="a"
+            href={recipe.recipe_url}
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
           >
             Original Recipe Link
-          </a>
-          <Typography variant="h3">
-            {this.props.recipeDetails[0].recipe_name}
-          </Typography>
-          <Typography>{`Total Cook Time: ${this.props.recipeDetails[0].total_time}`}</Typography>
-          <Typography>{`Servings: ${this.props.recipeDetails[0].serving_size}`}</Typography>
+          </Link>
+          <Typography variant="h3">{recipe.recipe_name}</Typography>
+          <Typography>{`Total Cook Time: ${recipe.total_time}`}</Typography>
+          <Typography>{`Servings: ${recipe.serving_size}`}</Typography>
           <Typography>
             Favorite: <FavoriteButton />
           </Typography>
-          <Typography>{this.props.recipeDetails[0].description}</Typography>
+          <Typography>{recipe.description}</Typography>
         </Grid>
         <Grid item xs={4}>
           <br />
           <img
-            src={this.props.recipeDetails[0].image_url}
-            alt={this.props.recipeDetails[0].recipe_name}
+            src={recipe.image_url}
+            alt={recipe.recipe_name}
             style={{ width: 250, height: 250 }}
           />
         </Grid>
